42ab53e77ad3742d338a91a6cc08f4d973de5533
yukimmm <yjedexxx@gmail.com>
2021-03-18 19:28:35 +0900

    product_create

---
+++ resources/views/mythrow_index.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{asset('asset/css/style.css')}}">
+</head>
+<body>
+    
+    <header class="logo_header">
+        <div class="logo">
+        throw_it
+        </div>
+    </header>
+    <main>
+        <div class="container">
+        <h1>MyPage</h1>
+    {{-- プロフィール？？ --}}
+
+    
+
+  
+
+    {{-- いいね商品一覧 --}}
+      <div class="throw_items">
+        <?php foreach ($items as $item): ?>
+        <div class="throw_items"> 
+            {{-- <img src=" echo 商品個別ページ写真？ " alt="カテゴリー"> --}}
+            {{-- <div class="items_name"><?php echo 商品個別ページ カテゴリ?></div> --}}
+            {{-- <div class="price"><?php echo 商品個別ページ price ?></div> --}}
+        </div>
+         @endforeach 
+      </div>
+      </div>
+    </main>
+
+</body>
+</html>
---
+++ resources/views/sell_item.blade.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{asset('asset/css/style.css')}}">
+</head>
+<body>
+    <header class="logo_header">
+        <div class="logo">
+        throw_it
+        </div>
+    </header>
+    <main>
+      <div class="container">
+        <div class="style_form item_photo" >出品画像</div>
+            <div class="photo_space"> 
+              <br>
+              <img src="{{asset('asset/img/baseline_add_a_photo_black_18dp.png')}}" alt="">
+              <br>
+              <p>アップロードするファイルを選択して下さい。</p>
+
+            </div>
+            <form action="#">
+              
+              <p><input type="file" name="s_file"></p>
+              </form>
+
+        <div class="style_form item_category">カテゴリー</div>
+          <div class="selectbox">
+            <select name="カテゴリー" >
+                <option value="ladies">選択してください</option>
+                <option value="ladies">レディ―ス</option>
+                <option value="ladies">メンズ</option>
+                <option value="ladies">その他</option>
+            </select>
+          </div>
+        <div class="style_form item_text">商品説明</div>
+        <div class="selectbox">
+        <textarea class="style_textarea" name="description" rows="7" placeholder="商品の説明（必須 1,000文字以内）
+        （色、素材、重さ、定価、注意点など）
+                  "></textarea>
+        </div>
+        <div class="style_form">販売価格</div>
+        <div class="selectbox">
+            <input class="item_price" type="number" name="price" placeholder="0" value=""  >
+            <span>円</span>
+        </div>    
+        <div class="style_form">
+          <button class="btn-primary" type="submit">出品する</button>
+
+        </div>
+      </div>  
+    </main>
+</body>
+</html>
