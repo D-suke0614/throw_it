454be287bcbb35a4361f23f19f86fa5b68f42c5b
watanabe masashi <msswtnb7@gmail.com>
2021-03-18 20:07:06 +0900

    toppage

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="./accets/css/style2.css">
+</head>
+<body>
+  <header>マイプロフィール</header>
+  <main>
+    <form class="profile" action="profile" method="post">
+      <div class="profile2">
+        名前:<input type="text" name="name"/>
+      </div>
+      <div class="profile2">
+        性別:<input type="text" name="sex"/>
+      </div>
+      <div class="profile2">
+        生年月日:<input type="text" name="birthday"/>
+      </div>
+      <div class="profile2">
+        年齢:<input type="text" name="age"/>
+      </div>
+      <div class="profile2">
+        メールアドレス:<input type="text" name="mail address"/>
+      </div>
+      <div class="profile2">
+        パスワード:<input type="text" name="password"/>
+      </div>
+      <input type="submit"/>
+    </form>
+  </main>
+</body>
+</html>
